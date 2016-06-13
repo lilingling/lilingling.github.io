58bdd611381582bce1b2cb6e595cd2514840d9a8
lilingling <15202276083@163.com>
2016-06-13 15:11:40 +0800

    update

--- works/gfmj/server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname + '/dist'));
-
-app.listen(8080, function(){
-    console.log('The server is running on PORT ' + 8080);
-});
